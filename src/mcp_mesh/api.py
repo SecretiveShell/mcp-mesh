27557f37e2a1f143656fb2030e35af2b51429f44
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-01-21 17:23:58 +0000

    add api routes

--- src/mcp_mesh/api.py
+++ src/mcp_mesh/api.py
@@ -1,5 +1,6 @@
 import json
 from fastapi import FastAPI
+import kademlia.node
 
 from mcp_mesh.lifespan import lifespan
 from mcp_mesh import server
@@ -12,8 +13,26 @@ async def read_root():
     return {"Hello": "World"}
 
 @app.get("/nodes")
-async def read_nodes():
-    return {"nodes": server.node.bootstrappable_neighbors()}
+async def read_nodes() -> list:
+    nodes = []
+    nodeitems = [bucket.nodes.values() for bucket in server.node.protocol.router.buckets]
+    item: kademlia.node.Node
+    for node in nodeitems:
+        for item in node:
+            nodes.append({
+                "ip": item.ip,
+                "id": item.id.hex(),
+                "port": item.port,
+            })
+
+    return nodes
+
+@app.get("/self")
+async def read_self():
+    return {
+        "self": server.node.node.id.hex(),
+        "bootstrap": config.bootstrap.bootstrap_node,
+    }
 
 @app.get("/config")
 async def read_config():
@@ -22,7 +41,7 @@ async def read_config():
 @app.get("/tools")
 async def read_tools():
     server.node.refresh_table()
-    
+
     tools = dict(server.node.storage.data).values()
     print(tools)
 
